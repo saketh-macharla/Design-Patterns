26125bce8874350456a131c33a4f11f44bcc4271
Saketh Macharla <saketh.macharla@zemosolabs.com>
2022-07-27 14:55:50 +0530

    abstractFactoryPattern

---
+++ abstractfactorypattern/demo/HDFC.java
@@ -0,0 +1,11 @@
+package abstractfactorypattern.demo;
+
+public class HDFC implements Bank{
+    private final String BNAME;
+    public HDFC(){
+        BNAME="HDFC BANK";
+    }
+    public String getBankName() {
+        return BNAME;
+    }
+}
---
+++ abstractfactorypattern/demo/LoanFactory.java
@@ -0,0 +1,21 @@
+package abstractfactorypattern.demo;
+
+public class LoanFactory extends AbstractFactory{
+    public Bank getBank(String bank){
+        return null;
+    }
+
+    public Loan getLoan(String loan){
+        if(loan == null){
+            return null;
+        }
+        if(loan.equalsIgnoreCase("Home")){
+            return new HomeLoan();
+        } else if(loan.equalsIgnoreCase("Business")){
+            return new BusinessLoan();
+        } else if(loan.equalsIgnoreCase("Education")){
+            return new EducationLoan();
+        }
+        return null;
+    }
+}
